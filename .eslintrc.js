79ed5ac248976617ef3177dbdb53c8107fcae309
chenyong <yong.chen@doone.com.cn>
2020-12-04 16:55:11 +0800

    细节修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,37 @@
+// https://eslint.org/docs/user-guide/configuring
+
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module'
+    parser: 'babel-eslint'
   },
   env: {
     browser: true,
-    node: true
   },
-  extends: 'standard',
-  globals: {
-    __static: true
-  },
-  plugins: [
-    'html'
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential', 
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
   ],
-  'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
     // allow async-await
-    'generator-star-spacing': 0,
+    'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-tabs': 0,
+    'no-mixed-spaces-and-tabs': 0,
+    'indent': ["off", "tab"],
+    'no-unused-vars': 0,
+    'no-trailing-spaces': 0,
+    "eqeqeq": 0,
+    'arrow-parens': 0,
+    'eol-last': ["error", "always"]
   }
 }
--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,17 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
-import { createPersistedState, createSharedMutations } from 'vuex-electron'
-
-import modules from './modules'
-
+import getters from './getters'
+import user from './modules/user'
+import common from './modules/common'
+// import { createPersistedState, createSharedMutations } from 'vuex-electron'
 Vue.use(Vuex)
-
-export default new Vuex.Store({
-  modules,
-  plugins: [
-    createPersistedState(),
-    createSharedMutations()
-  ],
-  strict: process.env.NODE_ENV !== 'production'
+const store = new Vuex.Store({
+  modules: {
+    user,
+    common
+  },
+  // plugins: [
+  //   createPersistedState(),
+  //   createSharedMutations()
+  // ],
+  // strict: process.env.NODE_ENV !== 'production',
+  getters
 })
+export default store
